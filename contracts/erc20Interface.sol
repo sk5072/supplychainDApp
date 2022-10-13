8ea344c9a05e80e7ab4fc2281379c7c6b5a305b1
Remix Workspace <>
2022-10-13 21:56:54 +0530

    Added supply chain functions

--- contracts/erc20Interface.sol
+++ contracts/erc20Interface.sol
@@ -6,14 +6,35 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract ERC20Interface {
     uint256 public totSupply;
-    
-    function totalSupply() public view returns (uint);
-    function balanceOf(address tokenOwner) public view returns (uint balance);
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    function totalSupply() public view returns (uint256);
+
+    function balanceOf(address tokenOwner)
+        public
+        view
+        returns (uint256 balance);
+
+    function allowance(address tokenOwner, address spender)
+        public
+        view
+        returns (uint256 remaining);
+
+    function transfer(address to, uint256 tokens) public returns (bool success);
+
+    function approve(address spender, uint256 tokens)
+        public
+        returns (bool success);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokens
+    ) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+    event Approval(
+        address indexed tokenOwner,
+        address indexed spender,
+        uint256 tokens
+    );
 }
--- contracts/erc20Token.sol
+++ contracts/erc20Token.sol
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------
-// ERC Token Standard #20 Interface
-// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+///Implements EIP20 token standard: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
 // ----------------------------------------------------------------------------
+
 pragma solidity >=0.4.21 <0.6.0;
 
 import "./erc20Interface.sol";
@@ -11,56 +11,81 @@ contract ERC20Token is ERC20Interface {
     mapping(address => uint256) public balances;
     mapping(address => mapping(address => uint256)) public allowed;
 
-    uint256 public totSupply;
-    string public name;
-    uint8 public decimals;
-    string public symbol;
+    uint256 public totSupply; // Total number of tokens
+    string public name; // Descriptive name (i.e. For Dummies Sample Token)
+    uint8 public decimals; // How many decimals to use when displaying amounts
+    string public symbol; // Short identifier for token (i.e. FDT)
 
+    // Create the new token and assign initial values, including initial amount
     constructor(
-        uint256 _initalAmount,
+        uint256 _initialAmount,
         string memory _tokenName,
         uint8 _decimalUnits,
         string memory _tokenSymbol
     ) public {
-        balances[msg.sender] = _initalAmount;
-        totSupply = _initalAmount;
-        name = _tokenName;
-        decimals = _decimalUnits;
-        symbol = _tokenSymbol;
+        balances[msg.sender] = _initialAmount; // The creator owns all initial tokens
+        totSupply = _initialAmount; // Update total token supply
+        name = _tokenName; // Store the token name (used for display only)
+        decimals = _decimalUnits; // Store the number of decimals (used for display only)
+        symbol = _tokenSymbol; // Store the token symbol (used for display only)
     }
 
-    function transfer(address _to, uint256 _value) public returns(bool success){
-        require(balances[msg.sender]>=_value, "Insufficeint funds to transfer source.");
-        balances[msg.sender] -=_value;
+    // Transfer tokens from msg.sender to a specified address
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(
+            balances[msg.sender] >= _value,
+            "Insufficient funds for transfer source."
+        );
+        balances[msg.sender] -= _value;
         balances[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
+        emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+    // Transfer tokens from one specified address to another specified address
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
         uint256 allowance = allowed[_from][msg.sender];
-        require(balances[_from]>=_value && allowance >= _value,"Insufficient funds for transder source.");
+        require(
+            balances[_from] >= _value && allowance >= _value,
+            "Insufficient allowed funds for transfer source."
+        );
         balances[_to] += _value;
-        balances[_from] -=_value;
-        if(allowance<MAX_UINT256){
+        balances[_from] -= _value;
+        if (allowance < MAX_UINT256) {
             allowed[_from][msg.sender] -= _value;
         }
-        emit Transfer(_from,_to,_value);
+        emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
         return true;
     }
 
-    function balanceOf(address _owner) public view returns(uint balance){
+    // Return the current balance (in tokens) of a specified address
+    function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
+    // Set
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
         return true;
     }
 
     // Return the
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256 remaining)
+    {
         return allowed[_owner][_spender];
     }
 
@@ -68,5 +93,4 @@ contract ERC20Token is ERC20Interface {
     function totalSupply() public view returns (uint256 totSupp) {
         return totSupply;
     }
-
 }
